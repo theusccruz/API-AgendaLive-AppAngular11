623d12ae528a62509709d426bb9640ff82ba9e4d
theusccruz <matheus8476@outlook.com>
2021-03-18 22:31:19 -0300

    exclusão de arquivos angular

--- agendaLiveAppTH/src/app/shared/model/live.model.ts
+++
@@ -1,9 +0,0 @@
-export class Live {
-    id: string;
-    liveName: string;
-    channelName: string;
-    liveDate: string;
-    liveTime: string;
-    liveLink: string;
-    registrationDate: string;
-}
